ff149a53f1c36944e79616946f537b3fd9c6691e
wei.he <wei.he@shuyun.com>
2020-08-06 17:54:50 +0800

    first commit

---
+++ SpringAuto/start/src/main/java/com/SpringAuto/SpringAutoApplication.java
@@ -0,0 +1,13 @@
+package com.SpringAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAutoApplication.class, args);
+    }
+
+}
---
+++ SpringAuto/start/src/test/java/com/SpringAuto/SpringAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringAuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
